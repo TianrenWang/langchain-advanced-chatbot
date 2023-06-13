26bb95f661cd89c5a96d70bf9173c5c2a6e883ef
Frank Wang <broodstar_frank@hotmail.com>
2023-06-12 22:47:47 -0400

    Can index documents on Pinecone

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Hello Sir");
+import * as dotenv from "dotenv";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { PineconeClient } from "@pinecone-database/pinecone";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+
+dotenv.config();
+
+// Create docs with a loader
+const loader = new TextLoader("sample_texts/HistoryOfUSA.txt");
+const docs = await loader.load();
+
+const client = new PineconeClient();
+await client.init({
+  apiKey: process.env.PINECONE_API_KEY,
+  environment: process.env.PINECONE_ENVIRONMENT,
+});
+const pineconeIndex = client.Index(process.env.PINECONE_INDEX);
+const embeddings = new OpenAIEmbeddings({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+});
+
+await PineconeStore.fromDocuments(docs, embeddings, {
+  pineconeIndex,
+});
+
+const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+  pineconeIndex,
+});
+
+/* Search the vector DB independently with meta filters */
+const results = await vectorStore.similaritySearch("Science", 1);
+console.log(results);
